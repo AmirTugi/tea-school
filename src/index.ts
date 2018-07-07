d5d4cf161b0ecc93eb8ce786c1052abea90083f6
Amir.Tugi@Gmail.com <Amir.T@logz.io>
2018-07-07 23:39:30 +0300

    Should be somehow ready now

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import pug, {LocalsObject as PugLocalsObject, Options as PugOptions} from 'pug';
+import * as sass from 'node-sass';
+import puppeteer, {NavigationOptions, PDFOptions} from 'puppeteer';
+import {Options as SassOptions} from "node-sass";
+
+namespace TeaSchool{
+    export interface GeneratePdfOptions {
+        styleOptions?: SassOptions;
+        htmlTemplatePath: string;
+        htmlTemplateOptions?: PugOptions & PugLocalsObject;
+        pdfOptions?: PDFOptions;
+    }
+
+    export const generatePdf = async (options: GeneratePdfOptions): Promise<Buffer> => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        const compiledStyle = sass.renderSync({...options.styleOptions});
+        const htmlTemplateOptions: PugOptions & PugLocalsObject = {...options.htmlTemplateOptions, compiledStyle};
+
+        const renderedTemplate = pug.renderFile(options.htmlTemplatePath, htmlTemplateOptions);
+
+        // Make puppeteer render the HTML from data buffer
+        await page.goto(`data:text/html,${renderedTemplate}`, {waitUntil: ['load', 'domcontentloaded']} as NavigationOptions);
+        const pdfBuffer = await page.pdf({...options.pdfOptions});
+
+        await browser.close();
+
+        return pdfBuffer
+    };
+}
+
+export default TeaSchool;
